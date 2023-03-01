d21654802f71658b5c4130edd1cc3b34f5557c31
Sresta Malla <mallasresta@gmail.com>
2023-03-01 16:53:34 +0545

    [task] imported css

--- src/App.js
+++ src/App.js
@@ -3,8 +3,18 @@ import "./App.css";
 
 // api key: 1dbb6316
 const API_URL = "http://www.omdbapi.com?apikey=1dbb6316";
+
 function App() {
-  useEffect(() => {}, []);
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
   return (
     <div className="App">
       <h1>Hello World</h1>
